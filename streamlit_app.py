5869167725bfca71b4479258967cad04a784ff6c
tanishq1402 <tanishq.midha@pixxel.co.in>
2024-12-18 15:46:50 +0530

    Updated description

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("MAPPER - Mass And Power Parameters Estimation Resource 🚀")
 st.write(
-    "This is an automated tool to calculate high-level system budgets for Pixxel's future missions and to generate technical proposals."
+    "This is an automated tool to calculate high-level system budgets such as mass, power and cost for Pixxel's future missions and to generate technical proposals."
 )
